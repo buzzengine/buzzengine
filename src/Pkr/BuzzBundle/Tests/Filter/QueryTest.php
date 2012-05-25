9b2dc558df94e1f6768d34490d455282fa197b24
Patrick Kromeyer <patrick.kromeyer@gmail.com>
2012-05-25 16:25:21 +0200

    query filter tests added

--- src/Pkr/BuzzBundle/Tests/Filter/QueryTest.php
+++ src/Pkr/BuzzBundle/Tests/Filter/QueryTest.php
@@ -12,38 +12,38 @@ class QueryTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Pkr\BuzzBundle\Filter\FilterInterface', $filter);
     }
 
-    public function testFilter()
+    public function testIsAccepted()
     {
-        $this->assertTrue($this->_testFilter('node.js', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr.'));
-        $this->assertTrue($this->_testFilter('node.js', 'Lorem ipsum dolor sit amet, NODE.JS sadipscing elitr.'));
-        $this->assertFalse($this->_testFilter('node.js', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'));
+        $this->assertTrue($this->_testIsAccepted('node.js', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr.'));
+        $this->assertTrue($this->_testIsAccepted('node.js', 'Lorem ipsum dolor sit amet, NODE.JS sadipscing elitr.'));
+        $this->assertFalse($this->_testIsAccepted('node.js', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'));
 
-        $this->assertTrue($this->_testFilter('node.js development', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr.'));
-        $this->assertTrue($this->_testFilter('node.js development', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr development.'));
-        $this->assertTrue($this->_testFilter('node.js development', 'Lorem development dolor sit amet, node.js sadipscing elitr.'));
-        $this->assertFalse($this->_testFilter('node.js development', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'));
-        $this->assertFalse($this->_testFilter('node.js development', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr.'));
-        $this->assertFalse($this->_testFilter('node.js development', 'Lorem ipsum dolor sit amet, development sadipscing elitr.'));
+        $this->assertTrue($this->_testIsAccepted('node.js development', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr.'));
+        $this->assertTrue($this->_testIsAccepted('node.js development', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr development.'));
+        $this->assertTrue($this->_testIsAccepted('node.js development', 'Lorem development dolor sit amet, node.js sadipscing elitr.'));
+        $this->assertFalse($this->_testIsAccepted('node.js development', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'));
+        $this->assertFalse($this->_testIsAccepted('node.js development', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr.'));
+        $this->assertFalse($this->_testIsAccepted('node.js development', 'Lorem ipsum dolor sit amet, development sadipscing elitr.'));
 
-        $this->assertTrue($this->_testFilter('"node.js development"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr.'));
-        $this->assertTrue($this->_testFilter('"node.js development"', 'Lorem ipsum dolor sit amet, node.js   development sadipscing elitr.'));
-        $this->assertTrue($this->_testFilter('"node.js development"', 'Lorem ipsum dolor sit amet, <em>node.js</em> development sadipscing elitr.'));
-        $this->assertFalse($this->_testFilter('"node.js development"', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr development.'));
-        $this->assertFalse($this->_testFilter('"node.js development"', 'Lorem development dolor sit amet, node.js sadipscing elitr.'));
-        $this->assertFalse($this->_testFilter('"node.js development"', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'));
-        $this->assertFalse($this->_testFilter('"node.js development"', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr.'));
-        $this->assertFalse($this->_testFilter('"node.js development"', 'Lorem ipsum dolor sit amet, development sadipscing elitr.'));
+        $this->assertTrue($this->_testIsAccepted('"node.js development"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr.'));
+        $this->assertTrue($this->_testIsAccepted('"node.js development"', 'Lorem ipsum dolor sit amet, node.js   development sadipscing elitr.'));
+        $this->assertTrue($this->_testIsAccepted('"node.js development"', 'Lorem ipsum dolor sit amet, <em>node.js</em> development sadipscing elitr.'));
+        $this->assertFalse($this->_testIsAccepted('"node.js development"', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr development.'));
+        $this->assertFalse($this->_testIsAccepted('"node.js development"', 'Lorem development dolor sit amet, node.js sadipscing elitr.'));
+        $this->assertFalse($this->_testIsAccepted('"node.js development"', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'));
+        $this->assertFalse($this->_testIsAccepted('"node.js development"', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr.'));
+        $this->assertFalse($this->_testIsAccepted('"node.js development"', 'Lorem ipsum dolor sit amet, development sadipscing elitr.'));
 
-        $this->assertTrue($this->_testFilter('node.js -npm', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr.'));
-        $this->assertFalse($this->_testFilter('node.js -npm', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr npm.'));
+        $this->assertTrue($this->_testIsAccepted('node.js -npm', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr.'));
+        $this->assertFalse($this->_testIsAccepted('node.js -npm', 'Lorem ipsum dolor sit amet, node.js sadipscing elitr npm.'));
 
-        $this->assertTrue($this->_testFilter('"node.js development" -"npm use"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr.'));
-        $this->assertTrue($this->_testFilter('"node.js development" -"npm use"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr use npm.'));
-        $this->assertFalse($this->_testFilter('"node.js development" -"npm use"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr npm use.'));
-        $this->assertFalse($this->_testFilter('"node.js development" -"npm use"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr npm   use.'));
+        $this->assertTrue($this->_testIsAccepted('"node.js development" -"npm use"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr.'));
+        $this->assertTrue($this->_testIsAccepted('"node.js development" -"npm use"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr use npm.'));
+        $this->assertFalse($this->_testIsAccepted('"node.js development" -"npm use"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr npm use.'));
+        $this->assertFalse($this->_testIsAccepted('"node.js development" -"npm use"', 'Lorem ipsum dolor sit amet, node.js development sadipscing elitr npm   use.'));
     }
 
-    protected function _testFilter($query, $content)
+    protected function _testIsAccepted($query, $content)
     {
         $filter = new Query();
         $filter->addQuery($query);
@@ -54,5 +54,48 @@ class QueryTest extends \PHPUnit_Framework_TestCase
                   ->will($this->returnValue($content));
 
         return $filter->isAccepted($mockEntry);
+    }
+
+    public function testIsAccepted2()
+    {
+        $filter = new Query();
+        $filter->addQuery('query');
+
+        $mockEntry = $this->getMock('Zend\Feed\Reader\Entry');
+        $mockEntry->expects($this->any())
+                  ->method('getTitle')
+                  ->will($this->returnValue('query'));
+        $mockEntry->expects($this->any())
+                  ->method('getDescription')
+                  ->will($this->returnValue('hello'));
+        $mockEntry->expects($this->any())
+                  ->method('getContent')
+                  ->will($this->returnValue('world'));
+
+        $this->assertTrue($filter->isAccepted($mockEntry));
+
+        $mockEntry->expects($this->any())
+                  ->method('getTitle')
+                  ->will($this->returnValue('hello'));
+        $mockEntry->expects($this->any())
+                  ->method('getDescription')
+                  ->will($this->returnValue('query'));
+        $mockEntry->expects($this->any())
+                  ->method('getContent')
+                  ->will($this->returnValue('world'));
+
+        $this->assertTrue($filter->isAccepted($mockEntry));
+
+        $mockEntry->expects($this->any())
+                  ->method('getTitle')
+                  ->will($this->returnValue('hello'));
+        $mockEntry->expects($this->any())
+                  ->method('getDescription')
+                  ->will($this->returnValue('world'));
+        $mockEntry->expects($this->any())
+                  ->method('getContent')
+                  ->will($this->returnValue('query'));
+
+        $this->assertTrue($filter->isAccepted($mockEntry));
     }
 }
