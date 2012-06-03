082a3b97f9b0279986909141ac3d9bf033a55c21
Patrick Kromeyer <patrick.kromeyer@gmail.com>
2012-06-03 19:22:04 +0200

    feed entries delete action added

--- src/Pkr/BuzzBundle/Controller/FeedEntryController.php
+++ src/Pkr/BuzzBundle/Controller/FeedEntryController.php
@@ -32,11 +32,13 @@ class FeedEntryController extends Controller
             throw $this->createNotFoundException('Unable to find Topic entity.');
         }
 
+        $deleteForm = $this->_createDeleteForm($id);
         $authors = $em->getRepository('PkrBuzzBundle:Author')->findByTopic($id);
 
         return array (
-            'topic'   => $topic,
-            'authors' => $authors
+            'topic'       => $topic,
+            'authors'     => $authors,
+            'delete_form' => $deleteForm->createView()
         );
     }
 
@@ -57,11 +59,55 @@ class FeedEntryController extends Controller
             throw $this->createNotFoundException('Unable to find Topic entity.');
         }
 
+        $deleteForm = $this->_createDeleteForm($id);
         $domains = $em->getRepository('PkrBuzzBundle:Domain')->findByTopic($id);
 
         return array (
-            'topic'   => $topic,
-            'domains' => $domains
+            'topic'       => $topic,
+            'domains'     => $domains,
+            'delete_form' => $deleteForm->createView()
         );
+    }
+
+    /**
+     * Deletes all Author, Domain and FeedEntry entities of a Topic.
+     *
+     * @Route("/topic/{id}/delete", name="feedEntry_delete")
+     * @Method("post")
+     */
+    public function deleteAction($id)
+    {
+        $form = $this->_createDeleteForm($id);
+        $request = $this->getRequest();
+
+        $form->bindRequest($request);
+
+        if ($form->isValid())
+        {
+            $em = $this->getDoctrine()->getEntityManager();
+
+            $authors = $em->getRepository('PkrBuzzBundle:Author')->findByTopic($id);
+            foreach ($authors as $author)
+            {
+                $em->remove($author);
+            }
+
+            $domains = $em->getRepository('PkrBuzzBundle:Domain')->findByTopic($id);
+            foreach ($domains as $domain)
+            {
+                $em->remove($domain);
+            }
+
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('feedEntry_author', array ('id' => $id)));
+    }
+
+    protected function _createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+                    ->add('id', 'hidden')
+                    ->getForm();
     }
 }
