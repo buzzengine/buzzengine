3a86d5bb3f388fd47b702510b332b29291dda564
Patrick Kromeyer <patrick.kromeyer@gmail.com>
2012-07-08 11:43:51 +0200

    routing cleanup

--- src/Pkr/BuzzBundle/Controller/DefaultController.php
+++ src/Pkr/BuzzBundle/Controller/DefaultController.php
@@ -4,17 +4,15 @@ namespace Pkr\BuzzBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @Route("/")
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return $this->redirect($this->generateUrl('topic'));
     }
 
     /**
